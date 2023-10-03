ca3bab83d3cc04633c03911ffa2c03edc1c5ffb9
yk22375006 <yk22375006@ga.ttc.ac.jp>
2023-10-03 15:37:26 +0900

    ガウス加工

--- 学園祭ゲーム/学園祭ゲーム/WinMain.cpp
+++ 学園祭ゲーム/学園祭ゲーム/WinMain.cpp
@@ -3,6 +3,7 @@
 
 int WINAPI WinMain(HINSTANCE hI,HINSTANCE hP,LPSTR lpC,int nC){
 	
+	int ScreenHandle;
 
 	float camera_direction = 0.0f;
 	
@@ -21,12 +22,13 @@ int WINAPI WinMain(HINSTANCE hI,HINSTANCE hP,LPSTR lpC,int nC){
 	ctgt = VGet(0.0f,500.0f,-400.0f) ;
 	cadd = VGet(0.0f, 0.0f, 0.0f);
 
-	ChangeWindowMode(TRUE);
-	SetGraphMode(960, 540, 32);
+	ChangeWindowMode(FALSE);
+	SetGraphMode(1920, 1080, 32);
 
 	// DXライブラリの初期化				DXライブラリースタート
 	if(DxLib_Init() == -1) return -1 ;
 
+	ScreenHandle = MakeScreen(1920, 1080, FALSE);
 
 
 	// ステージ情報の読み込み
@@ -87,7 +89,80 @@ int WINAPI WinMain(HINSTANCE hI,HINSTANCE hP,LPSTR lpC,int nC){
 	// シャドウマップに描画する範囲を設定
 	SetShadowMapDrawArea( ShadowMapHandle, VGet( -5000.0f, -10.0f, -5000.0f ), VGet( 5000.0f, 1000.0f, 5000.0f ) ) ;
 
-	SetDrawScreen(DX_SCREEN_BACK) ;
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	for (int i = 0; i < BACKGROUNDFLOOR; i++) {
+		bg_tatami[i] = MV1DuplicateModel(bgdate[BACKGROUNDTATAMI]);
+		MV1SetPosition(bg_tatami[i], VGet(1500.0f, 100.0f + (i * 800.0f), 100.0f));
+	}
+
+	blockcnt = 0;
+	// マップデータに反映
+	for (int y = MAP_Y - 1; y >= 0; y--) {
+		for (int x = 0; x < MAP_X; x++) {
+			if (StageMap[y][x] != 0) {
+				switch (StageMap[y][x]) {
+				case TATAMI_BLOCK:
+					m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[TATAMI_BLOCK]);
+					m_block[blockcnt].SetBlockType(TATAMI_BLOCK);
+					break;
+
+				case BREAK_BLOCK:
+					m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[BREAK_BLOCK]);
+					m_block[blockcnt].SetBlockType(BREAK_BLOCK);
+					break;
+
+				case FALL_BLOCK:
+					m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[FALL_BLOCK]);
+					m_block[blockcnt].SetBlockType(FALL_BLOCK);
+					break;
+
+				case INVINCIBLE_BLOCK:
+					m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[INVINCIBLE_BLOCK]);
+					m_block[blockcnt].SetBlockType(INVINCIBLE_BLOCK);
+					break;
+
+				case MOVE_BLOCK:
+					m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[MOVE_BLOCK]);
+					m_block[blockcnt].SetBlockType(MOVE_BLOCK);
+					m_block[blockcnt].SetMapPositionX(x);
+					break;
+
+				case NEEDLE_BLOCK:
+					m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[NEEDLE_BLOCK]);
+					m_block[blockcnt].SetBlockType(NEEDLE_BLOCK);
+					break;
+
+				case WOOD_BLOCK:
+					m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[WOOD_BLOCK]);
+					m_block[blockcnt].SetBlockType(WOOD_BLOCK);
+					break;
+				}
+				m_block[blockcnt].SetMapPositionY(y);
+				m_block[blockcnt].SetMapPositionX(x);
+				m_block[blockcnt].SetBlockFlag(TRUE);
+				blockcnt++;
+			}
+		}
+	}
+
+	blockcnt = 0;
+
+	for (int y = (MAP_Y - 1); y >= 0; y--) {
+		for (int x = 0; x < MAP_X; x++) {
+			if (StageMap[y][x] != 0) {
+				if (StageMap[y][x] != NEEDLE_BLOCK) {
+					MV1SetPosition(m_block[blockcnt].b_model, VGet((x * 200.0f), ((MAP_Y - y) * 100.0f), 0.0f));
+					m_block[blockcnt].SetBlockPosition(VGet((x * 200.0f), ((MAP_Y - y) * 100.0f), 0.0f));
+				}
+				else {
+					MV1SetPosition(m_block[blockcnt].b_model, VGet((x * 200.0f), ((MAP_Y - y) * 100.0f) + 50.0f, 0.0f));
+					m_block[blockcnt].SetBlockPosition(VGet((x * 200.0f), ((MAP_Y - y) * 100.0f) + 50.0f, 0.0f));
+				}
+				blockcnt++;
+			}
+		}
+	}
 
 	/* ------------------------------------------------------------------------------------------------
 												ゲームループ										
@@ -110,6 +185,11 @@ int WINAPI WinMain(HINSTANCE hI,HINSTANCE hP,LPSTR lpC,int nC){
 				break;
 
 			case eSceneChoice:
+				SetDrawScreen(ScreenHandle);
+
+				// 画面をクリア
+				ClearDrawScreen();
+
 				cpos = VGet(0.0f, 1000.0f, -1500.0f);
 				ctgt = VGet(0.0f, 2000.0f, 0.0f);
 				cadd = VGet(0.0f, 0.0f, 0.0f);
@@ -127,7 +207,56 @@ int WINAPI WinMain(HINSTANCE hI,HINSTANCE hP,LPSTR lpC,int nC){
 				MV1SetRotationXYZ(player[1].anim.model, VGet(0.0f, 1.57f * player[1].GetDirection(), 0.0f));
 				MV1SetPosition(player[1].anim.model, player[1].GetPosition());
 
-				Draw();
+				// シャドウマップへの描画の準備
+				ShadowMap_DrawSetup(ShadowMapHandle);
+
+				// モデルの描画 - 影の描画
+				MV1DrawModel(player[0].anim.model);
+				MV1DrawModel(player[1].anim.model);
+
+				// シャドウマップへの描画を終了
+				ShadowMap_DrawEnd();
+
+				// 描画に使用するシャドウマップを設定
+				SetUseShadowMap(0, ShadowMapHandle);
+
+				// 地面(配置)＆描画
+				MV1DrawModel(skydate);
+				MV1DrawModel(stagedate);
+				for (int i = 0; i < BACKGROUNDFLOOR; i++) {
+					MV1DrawModel(bg_tatami[i]);
+				}
+
+				blockcnt = 0;
+				for (blockcnt = 0; blockcnt < MAX_BLOCK; blockcnt++) {
+					if (m_block[blockcnt].GetBlockFlag() == TRUE) {
+						MV1DrawModel(m_block[blockcnt].b_model);
+					}
+				}
+
+				SetLightRangeAttenHandle(LHandle_p1, Range, Atten0, Atten1, Atten2);
+				SetLightAngleHandle(LHandle_p1, 0.24582103f, 6.28318548f);
+				SetLightPositionHandle(LHandle_p1, VGet(player[0].GetPosition().x, player[0].GetPosition().y + 100.0f, player[0].GetPosition().z - 500.0f)); // ライトの位置
+				SetLightPositionHandle(LHandle_p2, VGet(player[1].GetPosition().x, player[1].GetPosition().y + 100.0f, player[1].GetPosition().z - 500.0f)); // ライトの位置
+			//	SetLightDirectionHandle(LHandle_p1, VGet(ctgt.x = cpos.x , ctgt.y = cpos.y - 400   , ctgt.z = cpos.z ));  // ライトの方向
+			//	SetLightAngleHandle(LHandle_p1, 0.78f, 0.5f);
+
+				// 描画に使用するシャドウマップの設定を解除
+				SetUseShadowMap(0, -1);
+
+				// ８分の１に縮小した画像をガウスフィルタでぼかす
+				GraphFilter(ScreenHandle, DX_GRAPH_FILTER_GAUSS, 16, 1400);
+
+				// モデルの描画
+				MV1DrawModel(player[0].anim.model);
+				MV1DrawModel(player[1].anim.model);
+
+				// 描画対象を裏画面にする
+				SetDrawScreen(DX_SCREEN_BACK);
+
+				// 通常の描画結果を描画する
+				DrawGraph(0, 0, ScreenHandle, FALSE);
+
 				ScreenFlip();
 
 				if (CheckHitKey(KEY_INPUT_RETURN) == 1) {
@@ -135,78 +264,6 @@ int WINAPI WinMain(HINSTANCE hI,HINSTANCE hP,LPSTR lpC,int nC){
 					player[1].SetPosition(VGet(2800.0f, 5800.0f, 0.0f));
 					cpos = VGet(1484.0f, 2360.0f, -1860.0f);
 
-					for (int i = 0; i < BACKGROUNDFLOOR; i++) {
-						bg_tatami[i] = MV1DuplicateModel(bgdate[BACKGROUNDTATAMI]);
-						MV1SetPosition(bg_tatami[i], VGet(1500.0f, 100.0f + (i * 800.0f), 100.0f));
-					}
-
-					blockcnt = 0;
-					// マップデータに反映
-					for (int y = MAP_Y - 1; y >= 0; y--) {
-						for (int x = 0; x < MAP_X; x++) {
-							if (StageMap[y][x] != 0) {
-								switch (StageMap[y][x]) {
-									case TATAMI_BLOCK:
-										m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[TATAMI_BLOCK]);
-										m_block[blockcnt].SetBlockType(TATAMI_BLOCK);
-										break;
-
-									case BREAK_BLOCK:
-										m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[BREAK_BLOCK]);
-										m_block[blockcnt].SetBlockType(BREAK_BLOCK);
-										break;
-
-									case FALL_BLOCK:
-										m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[FALL_BLOCK]);
-										m_block[blockcnt].SetBlockType(FALL_BLOCK);
-										break;
-
-									case INVINCIBLE_BLOCK:
-										m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[INVINCIBLE_BLOCK]);
-										m_block[blockcnt].SetBlockType(INVINCIBLE_BLOCK);
-										break;
-
-									case MOVE_BLOCK:
-										m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[MOVE_BLOCK]);
-										m_block[blockcnt].SetBlockType(MOVE_BLOCK);
-										m_block[blockcnt].SetMapPositionX(x);
-										break;
-
-									case NEEDLE_BLOCK:
-										m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[NEEDLE_BLOCK]);
-										m_block[blockcnt].SetBlockType(NEEDLE_BLOCK);
-										break;
-
-									case WOOD_BLOCK:
-										m_block[blockcnt].b_model = MV1DuplicateModel(blockdate[WOOD_BLOCK]);
-										m_block[blockcnt].SetBlockType(WOOD_BLOCK);
-										break;
-								}
-								m_block[blockcnt].SetMapPositionY(y);
-								m_block[blockcnt].SetMapPositionX(x);
-								m_block[blockcnt].SetBlockFlag(TRUE);
-								blockcnt++;
-							}
-						}
-					}
-
-					blockcnt = 0;
-
-					for (int y = (MAP_Y - 1); y >= 0; y--) {
-						for (int x = 0; x < MAP_X; x++) {
-							if (StageMap[y][x] != 0) {
-								if (StageMap[y][x] != NEEDLE_BLOCK) {
-									MV1SetPosition(m_block[blockcnt].b_model, VGet((x * 200.0f), ((MAP_Y - y) * 100.0f), 0.0f));
-									m_block[blockcnt].SetBlockPosition(VGet((x * 200.0f), ((MAP_Y - y) * 100.0f), 0.0f));
-								}
-								else {
-									MV1SetPosition(m_block[blockcnt].b_model, VGet((x * 200.0f), ((MAP_Y - y) * 100.0f) + 50.0f, 0.0f));
-									m_block[blockcnt].SetBlockPosition(VGet((x * 200.0f), ((MAP_Y - y) * 100.0f) + 50.0f, 0.0f));
-								}
-								blockcnt++;
-							}
-						}
-					}
 					gamemode = eScenePlay;
 				}
 				break;
